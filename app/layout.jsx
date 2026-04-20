671185ae5b52fc3fd2f75fd86ab53a7f542aa4b4
Nritam Kumar <nritamk@gmail.com>
2026-04-20 15:29:53 +0530

    met tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,11 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+
+                <meta
+                    http-equiv="Content-Security-Policy"
+                    content="frame-ancestors 'self' https://*.netlify.app https://create.netlify.com;"
+                />
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
